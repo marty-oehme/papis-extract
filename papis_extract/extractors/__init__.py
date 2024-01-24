e7e5258b3486eff9ebe07c070521184627a039fc
Marty Oehme <marty.oehme@gmail.com>
2024-01-24 12:07:04 +0100

    feat: Only activate pocketbook extractor optionally
    Since we make the dependencies for pocketbook html extraction optional
    as an extra, this commit ensures the extractor (and cli option) only
    gets loaded when they exist.

--- papis_extract/extractors/__init__.py
+++ papis_extract/extractors/__init__.py
@@ -1,8 +1,19 @@
+import papis.logging
+
 from papis_extract.extraction import Extractor
 from papis_extract.extractors import pdf
 from papis_extract.extractors.pocketbook import PocketBookExtractor
 
+logger = papis.logging.get_logger(__name__)
+
 all_extractors: dict[str, Extractor] = {
     "pdf": pdf.PdfExtractor(),
-    "pocketbook": PocketBookExtractor(),
 }
+
+try:
+    import bs4
+    import magic
+
+    all_extractors["pocketbook"] = PocketBookExtractor()
+except ImportError:
+    logger.debug("pocketbook extractor not activated.")
